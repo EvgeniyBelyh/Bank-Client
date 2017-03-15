83d850038b0aa0ba52ba86ca15a1fc52b303b0a0
Belyh Evgeniy <jonik2004@bk.ru>
2017-03-15 08:42:28 +0300

    Исправил ошибки формирования блока меню операций со счетами

--- BankClient/src/java/ru/mti/bankclient/client/AccountOperationsMenuBlock.java
+++ BankClient/src/java/ru/mti/bankclient/client/AccountOperationsMenuBlock.java
@@ -35,16 +35,32 @@ public class AccountOperationsMenuBlock extends TransferMenuBlock {
         });
         // ссылка на страницу закрытия вклада
         Hyperlink closeDeposit = new Hyperlink();
-        closeDeposit.setText("Интернет");
+        closeDeposit.setText("Закрыть вклад");
         closeDeposit.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 //TODO открывать форму ввода данных
             }
         });
-        // ссылка на страницу блокировки карыты
+        // ссылка на страницу блокировки карты
         Hyperlink cardBlock = new Hyperlink();
-        cardBlock.setText("ЖКХ");
+        cardBlock.setText("Блокировать карту");
         cardBlock.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                //TODO открывать форму ввода данных
+            }
+        });
+        // ссылка на страницу выпуска виртуальной карты
+        Hyperlink issueVirtualCard = new Hyperlink();
+        issueVirtualCard.setText("Выпустить виртуальную карту");
+        issueVirtualCard.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                //TODO открывать форму ввода данных
+            }
+        }); 
+        // ссылка на страницу остории операций
+        Hyperlink operationsHistory = new Hyperlink();
+        operationsHistory.setText("Блокировать карту");
+        operationsHistory.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 //TODO открывать форму ввода данных
             }
@@ -53,6 +69,8 @@ public class AccountOperationsMenuBlock extends TransferMenuBlock {
         body.add(openDeposit);
         body.add(closeDeposit);
         body.add(cardBlock);
+        body.add(issueVirtualCard);
+        body.add(operationsHistory);
         // ставим стиль оформления
         body.setStyleName("menu_item");
         
